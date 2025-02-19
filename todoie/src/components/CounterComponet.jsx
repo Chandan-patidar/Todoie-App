aba3fdbaabe0d4c2f08eb4b4aa53d18566703920
Chandan Patidar <88586932+Chandan-patidar@users.noreply.github.com>
2025-02-19 10:07:39 +0530

    not working even odd function

--- todoie/src/components/CounterComponet.jsx
+++ todoie/src/components/CounterComponet.jsx
@@ -3,12 +3,16 @@ import React, { useState } from 'react';
 const CounterComponent = () => {
     const [count, setCount] = useState(15);
     
+    }
     return (
         <div>
             <p> Count Component - {count}</p>
-            <button onClick={() => setCount(45)}>Increment</button>
+            <h6> Number is  {count % 2 === 0 ? "Even" : "odd"} </h6>
+        
+            <button onClick={ checkOddOrEven}> Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
 
         </div>
-    )
+    );
 };
 export default CounterComponent;
